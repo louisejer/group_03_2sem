79f6f90ed8dfcde57945574c762ac599386dac1b
loui <loui@10.50.137.71>
2014-10-28 10:37:39 +0100

    første projekt udgave

---
+++ src/group_03_2sem/Group_03_2sem.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group_03_2sem;
+
+/**
+ *
+ * @author loui
+ */
+public class Group_03_2sem {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    
+}
